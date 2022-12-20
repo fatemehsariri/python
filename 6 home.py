e25a42c69ddc62d8b68f22c10e884226f23b9863
fatemehsariri <107997645+fatemehsariri@users.noreply.github.com>
2022-12-20 19:03:23 +0330

    Delete 6 home.py

--- 6 home.py
+++
@@ -1,7 +0,0 @@
-flag=0
-true_pass="Sagharsariri1998"
-while flag==0:
-    password=input("Enter password:")
-    if password==true_pass:
-        flag=1
-        print("Tada!You have entered the correct password!")
